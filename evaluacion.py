decdc5ae8ffdbb3285842623ebf0e914754bac2c
Yuli :] <yuliethgonzalez@liceovvh.cl>
2026-05-11 16:40:53 -0400

    .

--- evaluacion.py
+++ evaluacion.py
@@ -1,9 +1,23 @@
-"""Crear una función que reciba una lista de notas (decimales) y genere dos listas: una con aprobados (≥ 4.0) y otra con reprobados (< 4.0).
- Debe mostrar ambas listas y la cantidad de estudiantes en cada grupo.
+"""Crear una función que reciba una lista de números enteros y genere una nueva lista solo con los números pares mayores a 10.
+ Luego debe mostrar la nueva lista y la cantidad de elementos encontrados.
  """
  
-def listaNotas(lista):
-    pass
+def numParesMayor10(lista):
+    numMayor10 = []
 
-def ejerciciNotas():
-    pass
+    for i in range(0, len(lista)):
+        if lista[i] % 2 == 0 and lista[i] > 10:
+            numMayor10.append(str(lista[i]))
+
+    print(f"N° pares mayores a 10: {', '.join(numMayor10)}")
+
+def ejercicioNumPares():
+    limiteNum = int(input("Cuantos numeros quiere ingresar: "))
+    lista = []
+    for i in range(0, limiteNum):
+        num = int(input("Ingresa un num entero: "))
+        lista.append(num)
+   
+    numParesMayor10(lista)
+
+ejercicioNumPares()
