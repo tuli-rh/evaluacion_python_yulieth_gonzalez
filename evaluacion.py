1714a9bf8a83abf756a9ba5d67fc45776a58796d
Yuli :] <yuliethgonzalez@liceovvh.cl>
2026-05-11 16:21:49 -0400

    .

---
+++ evaluacion.py
@@ -0,0 +1,9 @@
+"""Crear una función que reciba una lista de notas (decimales) y genere dos listas: una con aprobados (≥ 4.0) y otra con reprobados (< 4.0).
+ Debe mostrar ambas listas y la cantidad de estudiantes en cada grupo.
+ """
+ 
+def listaNotas(lista):
+    pass
+
+def ejerciciNotas():
+    pass
